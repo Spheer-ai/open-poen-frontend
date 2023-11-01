67b4e517dec5d8724efbfa731f9fd91aa7f1be5d
Gerben <gerben.bosschieter@gmail.com>
2023-11-01 09:58:06 +0100

    change password fix

--- src/components/forms/ChangePasswordForm.tsx
+++ src/components/forms/ChangePasswordForm.tsx
@@ -7,7 +7,6 @@ import FormLayout from "./FormLayout";
 
 const ChangePasswordForm = ({ onClose, userId }) => {
   const { user } = useAuth();
-  const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState<string | null>(null);
@@ -28,10 +27,11 @@ const ChangePasswordForm = ({ onClose, userId }) => {
         return;
       }
 
-      const formData = new FormData();
-      formData.append("password", newPassword);
+      const formData = {
+        password: newPassword,
+      };
 
-      await updateUser(userId, formData, token, currentPassword, newPassword);
+      await updateUser(userId, formData, token);
 
       setIsConfirmed(true);
     } catch (error) {
@@ -59,27 +59,7 @@ const ChangePasswordForm = ({ onClose, userId }) => {
           </div>
         ) : (
           <form onSubmit={handleSubmit} className={styles["change-pass-form"]}>
-            <h3>Info</h3>
-            <div className={styles["form-group"]}>
-              <label
-                htmlFor="currentPassword"
-                className={styles["label-email"]}
-              >
-                Huidig wachtwoord:
-              </label>
-              <input
-                type={showPassword ? "text" : "password"}
-                id="currentPassword"
-                name="currentPassword"
-                value={currentPassword}
-                onChange={(e) => setCurrentPassword(e.target.value)}
-                required
-                className={styles["input"]}
-              />
-              <p className={styles["input-description"]}>
-                Voer uw huidige wachtwoord in.
-              </p>
-            </div>
+            <h3>Wachtwoord wijzigen</h3>
             <div className={styles["form-group"]}>
               <label htmlFor="newPassword" className={styles["label-email"]}>
                 Nieuw wachtwoord:
