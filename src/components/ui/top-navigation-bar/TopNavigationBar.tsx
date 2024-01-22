210111b3713f199222818f0691dc94ca28836e29
Gerben <gerben.bosschieter@gmail.com>
2024-01-22 09:46:30 +0100

    mobile wip

--- src/components/ui/top-navigation-bar/TopNavigationBar.tsx
+++ src/components/ui/top-navigation-bar/TopNavigationBar.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import styles from "../../../assets/scss/TopNavigationBar.module.scss";
 import Search from "../../elements/search/Search";
 import { TopNavigationBarProps } from "../../../types/TopNavigationBarType";
@@ -24,40 +25,98 @@ const TopNavigationBar: React.FC<TopNavigationBarProps> = ({
     }
   };
 
+  // Responsive behavior: Logo for screens below 768px
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className={styles["top-navigation-bar"]}>
-      <div className={styles["bar-items"]}>
-        {onBackArrowClick && (
-          <button onClick={onBackArrowClick} className={styles.backArrow}>
-            <img src="/arrow-left.png" alt="Terug" />
-          </button>
-        )}
-        <div
-          className={
-            hasBackArrow ? styles.titleWithArrow : styles.titleWithoutArrow
-          }
-          onClick={handleTitleClick}
-        >
-          <h2>{title}</h2>
-          {subtitle && (
-            <div className={styles.subtitle} style={subtitleStyle}>
-              {subtitle}
+      {windowWidth <= 768 && (
+        <div className={styles["top-section"]}>
+          <div className={styles["bar-items"]}>
+            <Link to="/funds">
+              <img
+                className={styles["logo"]}
+                src="/open-poen-logo-blue-mobile.svg"
+                alt="Home Logo"
+              />
+            </Link>
+            {showCta && hasPermission && (
+              <div className={styles["cta-button"]} onClick={onCtaClick}>
+                <button className={styles["cta-button"]}>+</button>
+              </div>
+            )}
+          </div>
+          <div className={styles["back-arrow-section"]}>
+            {onBackArrowClick && (
+              <button onClick={onBackArrowClick} className={styles.backArrow}>
+                <img src="/arrow-left.png" alt="Terug" />
+              </button>
+            )}
+            <div
+              className={
+                hasBackArrow ? styles.titleWithArrow : styles.titleWithoutArrow
+              }
+              onClick={handleTitleClick}
+            >
+              <h2>{title}</h2>
+              {subtitle && (
+                <div className={styles.subtitle} style={subtitleStyle}>
+                  {subtitle}
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
-        <div className={styles["icons"]}>
-          {showSettings && (
-            <div className={styles["settings-icon"]} onClick={onSettingsClick}>
-              <img src="/profile-settings.svg" alt="Profile Settings" />
-            </div>
+      )}
+      {windowWidth > 768 && (
+        <div className={styles["bar-items"]}>
+          {onBackArrowClick && (
+            <button onClick={onBackArrowClick} className={styles.backArrow}>
+              <img src="/arrow-left.png" alt="Terug" />
+            </button>
           )}
-          {showCta && hasPermission && (
-            <div className={styles["cta-button"]} onClick={onCtaClick}>
-              <button className={styles["cta-button"]}>+</button>
-            </div>
-          )}
+          <div
+            className={
+              hasBackArrow ? styles.titleWithArrow : styles.titleWithoutArrow
+            }
+            onClick={handleTitleClick}
+          >
+            <h2>{title}</h2>
+            {subtitle && (
+              <div className={styles.subtitle} style={subtitleStyle}>
+                {subtitle}
+              </div>
+            )}
+          </div>
+          <div className={styles["icons"]}>
+            {showSettings && (
+              <div
+                className={styles["settings-icon"]}
+                onClick={onSettingsClick}
+              >
+                <img src="/profile-settings.svg" alt="Profile Settings" />
+              </div>
+            )}
+            {showCta && hasPermission && (
+              <div className={styles["cta-button"]} onClick={onCtaClick}>
+                <button className={styles["cta-button"]}>+</button>
+              </div>
+            )}
+          </div>
         </div>
-      </div>
+      )}
       {showSearch && <Search onSearch={onSearch} />}
     </div>
   );
