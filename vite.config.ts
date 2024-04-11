775e540dde1b727b3e954aa8cf777090c383a701
Gerben <gerben.bosschieter@gmail.com>
2024-04-11 14:24:46 +0200

    added first name to add user

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 4000,
+    port: 8000,
     proxy: {
       '/api': {
         target: process.env.VITE_API_URL,
